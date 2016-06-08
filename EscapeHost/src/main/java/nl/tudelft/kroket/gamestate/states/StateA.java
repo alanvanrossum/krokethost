8b51ad16faa87ce62752903709ffb97b25704079
Irene van der Blij <i.c.vanderblij@student.tudelft.nl>
2016-06-08 13:01:27 +0200

    First interact with laptop before minigame B can start

--- EscapeHost/src/main/java/nl/tudelft/kroket/gamestate/states/StateA.java
+++ EscapeHost/src/main/java/nl/tudelft/kroket/gamestate/states/StateA.java
@@ -1,9 +1,12 @@
 package nl.tudelft.kroket.gamestate.states;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import nl.tudelft.kroket.gamestate.GameState;
 import nl.tudelft.kroket.log.Logger;
+import nl.tudelft.kroket.net.protocol.CommandParser;
+import nl.tudelft.kroket.net.protocol.Protocol;
 
 /**
  * This is the first game state. In this game state minigame A can be started and ended. The game
@@ -30,6 +33,46 @@ public class StateA extends GameState {
     return STATE_NAME;
   }
 
+  @Override
+  public void stop() {
+	    log.info(className, "Stopping gameState " + getName());
+	    host.sendAll(String.format("%s[%s]", Protocol.COMMAND_DONE, getName()));
+	  }
+  
+  /**
+   * Advance to the next gamestate.
+   */
+  private void advance() {
+	    setActive(false);
+	    session.advance();
+  }
+  
+  @Override
+  public void handleInput(String input, HashMap<String, String> parsedInput) {
+
+	    log.info(className, "handleInput in state " + getName());
+
+	    if (!parsedInput.containsKey("param_0") || !parsedInput.get("param_0").equals(getName())) {
+	      return;
+	    }
+
+	    if (!isActive()) {
+	      if (parsedInput.get("command").equals("BEGIN")) {
+	        host.sendAll(input);
+	        start();
+	      }
+	    } else if (parsedInput.get("command").equals("DONE")) {
+	    	if (parsedInput.containsKey("param_1")) {
+	    		advance();
+	    		return;
+	    	}
+	    		
+	      host.sendAll(input);
+	      stop();
+	    } else {
+	      log.info(className, "Input ignored.");
+	    }
+	  }
   
 
 }
