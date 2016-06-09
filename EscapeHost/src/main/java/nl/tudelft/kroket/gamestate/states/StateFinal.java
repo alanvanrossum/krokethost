e06b9d499b137d70584f5cb93be858a305163543
Irene van der Blij <i.c.vanderblij@student.tudelft.nl>
2016-06-09 12:47:49 +0200

    send gamewon message to all in final state

--- EscapeHost/src/main/java/nl/tudelft/kroket/gamestate/states/StateFinal.java
+++ EscapeHost/src/main/java/nl/tudelft/kroket/gamestate/states/StateFinal.java
@@ -1,18 +1,36 @@
 package nl.tudelft.kroket.gamestate.states;
 
 import nl.tudelft.kroket.gamestate.GameState;
+import nl.tudelft.kroket.log.Logger;
+import nl.tudelft.kroket.net.protocol.Protocol;
 
 /**
  * This is the final game state. In this state the game is won.
  */
 public class StateFinal extends GameState {
 
+  /** Singleton reference to logger. */
+  static final Logger log = Logger.getInstance();
+  
+  /** Class simpleName, used as tag for logging. */
+  private final String className = this.getClass().getSimpleName();
+  
   private static final String STATE_NAME = "FINAL";
 
   private static GameState instance = new StateFinal();
 
   public static GameState getInstance() {
     return instance;
+  }
+  
+   /**
+   * Start a state.
+   */
+  @Override
+  protected void start() {
+    log.info(className, "Starting gameState " + getName());
+    setActive(true);
+    host.sendAll(Protocol.COMMAND_GAMEWON);
   }
 
   @Override
